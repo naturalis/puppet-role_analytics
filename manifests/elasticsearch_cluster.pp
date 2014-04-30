cf0f8520b5fcf0285a4e213378b7259c2058c2ba
Atze de Vries <atze.devries@naturalis.nl>
2014-04-30 17:08:06 +0200

    change in exporting

--- manifests/elasticsearch_cluster.pp
+++ manifests/elasticsearch_cluster.pp
@@ -48,11 +48,11 @@ class role_analytics::elasticsearch_cluster(
     module_dir => 'head',
   }
 
-  @@role_analytics::logstash_indexer::indexer_config { "elasticsearch_output_${cluster_name}" :
-    type    => 'output',
-    content => "elasticsearch bla",
-    tag     => "${cluster_name}_indexer_config",
-  }
+  #@@role_analytics::logstash_indexer::indexer_config { "elasticsearch_output_${cluster_name}" :
+  #  type    => 'output',
+  #  content => "elasticsearch bla",
+  #  tag     => "${cluster_name}_indexer_config",
+  #}
 
   #@@file_fragment { "elasticsearch_output_${cluster_name}" :
   #    tag     => "LS_CONFIG_${cluster_name}",
