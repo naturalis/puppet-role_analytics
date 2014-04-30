c927638a10da0d699717d58eb9200ae1f3a278da
Atze de Vries <atze.devries@naturalis.nl>
2014-04-30 14:08:00 +0200

    rewriten file concat

--- manifests/logstash_indexer.pp
+++ manifests/logstash_indexer.pp
@@ -2,6 +2,7 @@ class role_analytics::logstash_indexer(
 	$version                       = '1.4',
 ){
 
+  $cluster_name = $role_analytics::params::cluster_name
 
   apt::source { 'logstash':
     location    => "http://packages.elasticsearch.org/logstash/${version}/debian",
@@ -32,38 +33,48 @@ class role_analytics::logstash_indexer(
 
 
 
-  file_fragment { '/etc/logstash/conf.d/logstash_indexer.conf':
-      tag     => "LS_CONFIG_${::fqdn}",
+  file_fragment { 'begin input':
+      tag     => "LS_CONFIG_${cluster_name}",
       content => 'input {',
       order   => 0,
   }
-  file_fragment { '/etc/logstash/conf.d/logstash_indexer.conf':
-      tag     => "LS_CONFIG_${::fqdn}",
+  file_fragment { 'end_input':
+      tag     => "LS_CONFIG_${cluster_name}",
       content => '}',
       order   => 398,
   }
-  file_fragment { '/etc/logstash/conf.d/logstash_indexer.conf':
-      tag     => "LS_CONFIG_${::fqdn}",
+  file_fragment { 'begin filter':
+      tag     => "LS_CONFIG_${cluster_name}",
       content => 'filter {',
       order   => 399,
   }
-  file_fragment { '/etc/logstash/conf.d/logstash_indexer.conf':
-      tag     => "LS_CONFIG_${::fqdn}",
+  file_fragment { 'end filter':
+      tag     => "LS_CONFIG_${cluster_name}",
       content => '}',
       order   => 698,
   }
-  file_fragment { '/etc/logstash/conf.d/logstash_indexer.conf':
-      tag     => "LS_CONFIG_${::fqdn}",
+  file_fragment { 'begin output':
+      tag     => "LS_CONFIG_${cluster_name}",
       content => 'output {',
       order   => 699,
   }
-  file_fragment { '/etc/logstash/conf.d/logstash_indexer.conf':
-      tag     => "LS_CONFIG_${::fqdn}",
+  file_fragment { 'end output':
+      tag     => "LS_CONFIG_${cluster_name}",
       content => '}',
       order   => 999,
   }
 
 
+  File_fragment <<| tag == "LS_CONFIG_${cluster_name}" |>>
+
+  file_concat { '/etc/logstash/conf.d/indexer':
+    tag     => "LS_CONFIG_${cluster_name}", # Mandatory
+    owner   => 'logstash',       # Optional. Default to root
+    group   => 'logstash',       # Optional. Default to root
+    mode    => '0640',        # Optional. Default to 0644
+    require => Package['logstash'],
+    notify  => Service['logstash'],
+  }
 
 
   define indexer_config(
@@ -83,8 +94,8 @@ class role_analytics::logstash_indexer(
     }
 
 
-    file_fragment { '/etc/logstash/conf.d/logstash_indexer.conf':
-      tag     => "LS_CONFIG_${::fqdn}",
+    file_fragment { $name :
+      tag     => "LS_CONFIG_${cluster_name}",
       content => $content,
       order   => $order,
       notify  => Service['logstash'],
