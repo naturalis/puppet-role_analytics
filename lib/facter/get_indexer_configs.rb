8cb21b58566393d9f5d5369b58cccdb65bce73c2
Atze de Vries <atze.devries@naturalis.nl>
2014-05-09 14:42:10 +0200

    some testing with facts

--- lib/facter/get_indexer_configs.rb
+++ lib/facter/get_indexer_configs.rb
@@ -2,9 +2,11 @@ Facter.add("indexer_templates") do
   setcode do
     dir = "/etc/logstash/conf.d"
     if File.directory?(dir)
-      result = Dir.entries(dir)
+      filters = Dir.entries(dir)
+      filters.reject! {|x| x == "." }
+      filters.reject! {|x| x == ".." }
       #Facter::Util::Resolution.exec('/bin/ls /etc/logstash/conf.d')
-      puts result.join(",")
+      puts filters.join(",")
     else
       puts "none"
     end
