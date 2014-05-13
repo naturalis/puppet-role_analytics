9d137591228a4a41ddf4347dd58dfd9c5ca42617
Atze de Vries <atze.devries@naturalis.nl>
2014-05-13 16:32:47 +0200

    change to durable

--- manifests/rabbit_mq.pp
+++ manifests/rabbit_mq.pp
@@ -26,7 +26,6 @@ class role_analytics::rabbit_mq (
     password => $rabbit_logstash_password,
     type     => 'direct',
     ensure   => present,
-    durable  => 'false',
   }
 
   #file_fragment { 'begin output':
