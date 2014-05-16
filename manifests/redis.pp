1db9ab5c5e28103c1b123bf3be055738ca4f9540
Atze de Vries <atze.devries@naturalis.nl>
2014-05-16 12:04:07 +0200

    some nice stuff

--- manifests/redis.pp
+++ manifests/redis.pp
@@ -16,8 +16,8 @@ class role_analytics::redis (
     tag           => "LS_CONFIG_INDEXER${cluster_name}",
   }
 
-  $test_q = query_nodes('Class[Role_analytics::Elasticsearch_cluster')
-  $test_w = query_facts('Class[Role_analytics::Elasticsearch_cluster',['ipaddress','cluster_name'])
+  $test_q = query_nodes('Class[Role_analytics::Elasticsearch_cluster]')
+  $test_w = query_facts('Class[Role_analytics::Elasticsearch_cluster]',['ipaddress','cluster_name'])
 
   notify { $test_q : }
   notify { $test_w : }
