61dfdfe6768304c764f8b6f1739b67d93633cf7d
Atze de Vries <atze.devries@naturalis.nl>
2014-05-16 11:59:16 +0200

    some nice stuff

--- manifests/logstash_client.pp
+++ manifests/logstash_client.pp
@@ -1,14 +1,14 @@
 class role_analytics::logstash_indexer(
   $cluster_name,
 	$version          = '1.4',
-  $logstash_filter  = '# No Filter Configured',
+  $logstash_input   = '# No input configured. Use Puppet variable',
+  $logstash_filter  = '# No filter configured. Use Puppet variable',
 ){
 
   apt::source { 'logstash':
     location    => "http://packages.elasticsearch.org/logstash/${version}/debian",
     release     => 'stable',
     repos       => 'main',
-#    key         => '2BF6ED30',
     key_server  => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
     include_src => false,
   }
@@ -26,46 +26,54 @@ class role_analytics::logstash_indexer(
 
 
   file_fragment { 'begin input':
-      tag     => "LS_CONFIG_INDEXER_${cluster_name}",
+      tag     => "LS_CONFIG_CLIENT_${cluster_name}",
       content => 'input {
 ',
       order   => 0,
   }
+
+  file_fragment { 'filter':
+    tag     => "LS_CONFIG_CLIENT_${cluster_name}",
+    content => $logstash_input,
+    order   => 200,
+  }
+
   file_fragment { 'end_input':
-      tag     => "LS_CONFIG_INDEXER_${cluster_name}",
+      tag     => "LS_CONFIG_CLIENT_${cluster_name}",
       content => '}
 ',
       order   => 398,
   }
   file_fragment { 'begin filter':
-      tag     => "LS_CONFIG_INDEXER_${cluster_name}",
+      tag     => "LS_CONFIG_CLIENT_${cluster_name}",
       content => 'filter {
 ',
       order   => 399,
   }
   file_fragment { 'filter':
-    tag     => "LS_CONFIG_INDEXER_${cluster_name}",
+    tag     => "LS_CONFIG_CLIENT_${cluster_name}",
     content => $logstash_filter,
     order   => 500,
   }
   file_fragment { 'end filter':
-      tag     => "LS_CONFIG_INDEXER_${cluster_name}",
+      tag     => "LS_CONFIG_CLIENT_${cluster_name}",
       content => '}
 ',
       order   => 698,
   }
-  file_fragment { 'output':
-      tag     => "LS_CONFIG_INDEXER_${cluster_name}",
-      content => "output { elasticsearch { cluster => '${cluster_name}' } }",
-      order   => 699,
+
+  #file_fragment { 'output':
+  #    tag     => "LS_CONFIG_CLIENT_${cluster_name}",
+  #    content => "output { elasticsearch { cluster => '${cluster_name}' } }",
+  #    order   => 699,
+  #}
+
+  File_fragment <<| tag == "LS_CONFIG_CLIENT_${cluster_name}" |>> {
+    before => File_concat['/etc/logstash/conf.d/logstash_client.conf']
   }
 
-  File_fragment <<| tag == "LS_CONFIG_INDEXER_${cluster_name}" |>> {
-    before => File_concat['/etc/logstash/conf.d/logstash_indexer.conf']
-  }
-
-  file_concat { '/etc/logstash/conf.d/logstash_indexer.conf':
-    tag     => "LS_CONFIG_INDEXER_${cluster_name}", # Mandatory
+  file_concat { '/etc/logstash/conf.d/logstash_client.conf':
+    tag     => "LS_CONFIG_CLIENT_${cluster_name}", # Mandatory
     owner   => 'logstash',       # Optional. Default to root
     group   => 'logstash',       # Optional. Default to root
     mode    => '0640',        # Optional. Default to 0644
--- manifests/redis.pp
+++ manifests/redis.pp
@@ -8,7 +8,19 @@ class role_analytics::redis (
     manage_repo => true,
     ppa_repo    => 'ppa:rwky/redis',
     requirepass => $redis_password,
-    #masterauth  => $redis_password,
   }
+
+  @@file_fragment { "logstash-input-${::fqdn}":
+    order          => 100,
+    content       => template('role_analytics/logstash_redis_input.erb'),
+    tag           => "LS_CONFIG_INDEXER${cluster_name}",
+  }
+
+  $test_q = query_nodes('Class[Role_analytics::Elasticsearch_cluster]')
+  $test_w = query_facts('Class[Role_analytics::Elasticsearch_cluster]',['ipaddress','cluster_name'])
+
+  notify { $test_q : }
+  notify { $test_w : }
+
 
 }
