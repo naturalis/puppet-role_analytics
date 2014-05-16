3a7713aa691e4f4433a04f7083cb1bf7c8b795e1
Atze de Vries <atze.devries@naturalis.nl>
2014-05-16 13:45:35 +0200

    some nice stuff

--- manifests/redis.pp
+++ manifests/redis.pp
@@ -19,11 +19,11 @@ class role_analytics::redis (
   $test_q = query_nodes('Class[Role_analytics::Elasticsearch_cluster]')
   $test_w = query_facts('Class[Role_analytics::Elasticsearch_cluster]',['ipaddress','cluster_name'])
 
-  #notify { $test_q :
-  #  message => $test_w[ipaddress],
-  #}
-  $test_x = parsejson($test_w)
-  notify { $test_x : }
+  notify { $test_q :
+    message => parsejson($test_w[$title]),
+  }
+  #$test_x = parsejson($test_w)
+  #notify { $test_x : }
 
 
 }
