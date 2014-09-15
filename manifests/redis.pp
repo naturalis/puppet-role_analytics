17db79c00b44922c8967a4d599d1d3c6dcdeacf5
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-09-15 11:44:09 +0200

    added memory overcommit

--- manifests/redis.pp
+++ manifests/redis.pp
@@ -1,22 +1,26 @@
 class role_analytics::redis (
-  $cluster_name,
-  $redis_password,
+  $cluster_name   = undef,
+  $redis_password = undef,
 ) {
 
   class { '::redis':
-    bind        => $::ipaddress,
-    manage_repo => true,
-    ppa_repo    => 'ppa:rwky/redis',
-    requirepass => $redis_password,
+    bind          => $::ipaddress,
+    manage_repo   => true,
+    ppa_repo      => 'ppa:rwky/redis',
+    requirepass   => $redis_password,
   }
 
   @@file_fragment { "logstash-input-${::fqdn}":
-    order          => 100,
+    order         => 100,
     content       => template('role_analytics/logstash_redis_input.erb'),
     tag           => "LS_CONFIG_INDEXER_${cluster_name}",
   }
 
-
-
-
+  file { '/etc/sysctl.conf':
+    ensure        => present,
+    }
+  file_line { 'Add vm overcommit_memory to /etc/sysctl.conf':
+    path          => '/etc/sysctl.conf',
+    line          => 'vm.overcommit_memory=1',
+  }
 }
