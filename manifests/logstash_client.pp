7dc5132a5e09c22b4daa670f0b7df227285e7c80
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-09-22 13:01:22 +0200

    adding centos support

--- manifests/logstash_client.pp
+++ manifests/logstash_client.pp
@@ -183,23 +183,23 @@ class role_analytics::logstash_client(
 
       if $use_collectd {
 
-        class { '::collectd':
-          purge                 => true,
-          recurse               => true,
-          purge_config          => true,
-        }
+#        class { '::collectd':
+#          purge                 => true,
+#          recurse               => true,
+#          purge_config          => true,
+#        }
 
-        class { 'collectd::plugin::network':
-          server                => '127.0.0.1',
-        }
-        class { 'collectd::plugin::load': }
-        class { 'collectd::plugin::memory': }
-        class { 'collectd::plugin::disk':
-          disks                 => $collectd_disks,
-        }
-        class { 'collectd::plugin::interface': }
-        class { 'collectd::plugin::df': }
-        class {'collectd::plugin::uptime': }
+#        class { 'collectd::plugin::network':
+#          server                => '127.0.0.1',
+#        }
+#        class { 'collectd::plugin::load': }
+#        class { 'collectd::plugin::memory': }
+#        class { 'collectd::plugin::disk':
+#          disks                 => $collectd_disks,
+#        }
+#        class { 'collectd::plugin::interface': }
+#        class { 'collectd::plugin::df': }
+#        class {'collectd::plugin::uptime': }
 
         file_fragment { 'input collectd':
           tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
