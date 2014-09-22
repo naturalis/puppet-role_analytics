c7e5b2f46ed1e1cd64c88e9caa483a60a7a5814a
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-09-22 12:57:15 +0200

    adding centos support

--- manifests/logstash_client.pp
+++ manifests/logstash_client.pp
@@ -164,6 +164,9 @@ class role_analytics::logstash_client(
       }
     }
     "CentOS":  {
+    # $redis_cluster_members = query_nodes("Class[Role_analytics::Redis]{cluster_name='${cluster_name}'}",ec2_public_ipv4)
+    # $redis_cluster_string = join($redis_cluster_members,'","')
+      $redis_cluster_string = ("10.42.1.118","10.42.1.116","10.42.1.117")
 
       yumrepo { 'logstash':
         descr    => 'Logstash Centos Repo',
@@ -178,7 +181,132 @@ class role_analytics::logstash_client(
         require                 => yumrepo['logstash'],
       }
 
-      notify { "Logging is not working with CentOS '$operatingsystemrelease' so disabled": }
+      if $use_collectd {
+
+        class { '::collectd':
+          purge                 => true,
+          recurse               => true,
+          purge_config          => true,
+        }
+
+        class { 'collectd::plugin::network':
+          server                => '127.0.0.1',
+        }
+        class { 'collectd::plugin::load': }
+        class { 'collectd::plugin::memory': }
+        class { 'collectd::plugin::disk':
+          disks                 => $collectd_disks,
+        }
+        class { 'collectd::plugin::interface': }
+        class { 'collectd::plugin::df': }
+        class {'collectd::plugin::uptime': }
+
+        file_fragment { 'input collectd':
+          tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
+          content               => '  collectd { tags => ["collectd"] }
+    ',
+          order                 => 100,
+        }
+      }
+
+      service {'logstash':
+        ensure                  => running,
+        enable                  => true,
+        require                 => Package['logstash'],
+        hasrestart              => true,
+      }
+
+      file_fragment { 'begin input':
+          tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
+          content               => 'input {
+    ',
+          order                 => 0,
+      }
+
+      file_fragment { 'input':
+        tag                     => "LS_CONFIG_CLIENT_${cluster_name}",
+        content                 => $logstash_input,
+        order                   => 200,
+      }
+
+      file_fragment { 'end_input':
+          tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
+          content               => '
+    }
+    ',
+          order                 => 398,
+      }
+
+      file_fragment { 'begin filter':
+          tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
+          content               => 'filter {
+    ',
+          order                 => 399,
+      }
+
+      file_fragment { 'filter':
+        tag                     => "LS_CONFIG_CLIENT_${cluster_name}",
+        content                 => $logstash_filter,
+        order                   => 500,
+      }
+
+      file_fragment { 'end filter':
+          tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
+          content               => '
+    }
+    ',
+          order                 => 698,
+      }
+
+      file_fragment { 'output':
+          tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
+          content               => template('role_analytics/logstash_redis_output.erb'),
+          order                 => 699,
+      }
+
+      File_fragment <<| tag == "LS_CONFIG_CLIENT_${cluster_name}" |>> {
+        before                  => File_concat['/etc/logstash/conf.d/logstash_client.conf']
+      }
+
+      file_concat { '/etc/logstash/conf.d/logstash_client.conf':
+        tag                     => "LS_CONFIG_CLIENT_${cluster_name}",
+        owner                   => 'logstash',
+        group                   => 'logstash',
+        mode                    => '0640',
+        require                 => Package['logstash'],
+        notify                  => Service['logstash'],
+      }
+
+      file_line { 'syslog_workaround':
+        ensure                  => "present",
+        require                 => Package['logstash'],
+        path                    => '/etc/init/logstash.conf',
+        match                   => 'setgid',
+        line                    => 'setgid adm',
+        notify                  => Exec['update_groups'],
+      }
+
+      exec { 'update_groups':
+        command                 => "/usr/sbin/usermod -a -G adm logstash && /etc/init.d/logstash restart && /etc/init.d/collectd restart",
+        refreshonly             => true,
+        require                 => Package['logstash'],
+        unless                  => "/usr/bin/groups logstash | grep adm"
+      }
+
+      if $use_dashboard {
+        file {"/tmp/${dashboard_name}.json":
+          ensure                => "present",
+          mode                  => "644",
+          content               => template("role_analytics/${dashboard_name}.json.erb"),
+          notify                => Exec['install_dashboard'],
+      }
+        exec { 'install_dashboard':
+          command               => "/usr/bin/curl -XPUT http://${kibana_ip}:9200/kibana-int/dashboard/host-${hostname} -T /tmp/${dashboard_name}.json",
+          refreshonly           => true,
+        }
+      }
+
+    #  notify { "Logging is not working with CentOS '$operatingsystemrelease' so disabled": }
     }
 
     "default":  {
