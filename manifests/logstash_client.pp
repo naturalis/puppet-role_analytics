bd450a69db14596e8ea35e28db7bf86cb1e8df37
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-09-23 09:40:18 +0200

    adding centos support

--- manifests/logstash_client.pp
+++ manifests/logstash_client.pp
@@ -65,9 +65,9 @@ if ! defined(Class["role_analytics::logstash_indexer"]) {
               purge_config          => true,
             }
 
-#            class { '::collectd::plugin::network':
-#              server       =>  '127.0.0.1',
-#            }
+            class { '::collectd::plugin::network':
+              server       =>  '127.0.0.1',
+            }
 
             class { 'collectd::plugin::load': }
             class { 'collectd::plugin::memory': }
