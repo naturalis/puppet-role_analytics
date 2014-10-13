4c8172c4419ad4181d771dbc62a61ca05a3d3878
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-10-13 16:59:24 +0200

    testing improved logstash client

--- manifests/logstash_client2.pp
+++ manifests/logstash_client2.pp
@@ -14,8 +14,7 @@ class role_analytics::logstash_client2(
   $host_specific                    = undef,
   $config_hash = {
      'LS_HEAP_SIZE' => '200m',
-     'LS_USER' => 'logstash',
-     'LS_GROUP' => 'logstash',
+     'setgid' => 'adm',
    }
 ){
 
