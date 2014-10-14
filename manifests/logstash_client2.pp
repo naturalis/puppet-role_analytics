57e062a8371a26b46f6b516aedd5c25399b65813
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-10-14 08:32:28 +0200

    fixing syslog

--- manifests/logstash_client2.pp
+++ manifests/logstash_client2.pp
@@ -55,6 +55,9 @@ class role_analytics::logstash_client2(
           },
         },
       }
+      class { 'collectd::plugin::syslog':
+        log_level => 'info'
+      }
 
       file_fragment { 'input collectd':
         tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
