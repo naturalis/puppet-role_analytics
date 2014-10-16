835ffb502ea8701bae770c1832bdaa00a60568b9
Foppe Pieters <foppe.pieters@naturalis.nl>
2014-10-16 13:23:43 +0200

    added stages

--- manifests/logstash_client2.pp
+++ manifests/logstash_client2.pp
@@ -1,30 +1,22 @@
 class role_analytics::logstash_client2(
 
-  $cluster_name                     = undef,
-  $redis_password                   = undef,
-  $version                          = '1.4',
-  $logstash_input                   = '',
-  $logstash_filter                  = '',
-  $use_collectd                     = true,
-  $collectd_disks                   = ['dm-2'],
-  $use_dashboard                    = true,
-  $dashboard_name                   = 'host-baseboard',
-  $kibana_ip                        = '10.42.1.111',
-  $redis_ip                         = ["10.42.1.118","10.42.1.116","10.42.1.117"],
-  $host_specific                    = undef,
-  $config_hash = {
-     'LS_HEAP_SIZE' => '200m',
-     'LS_USER'      => 'root',
-  }
+  $cluster_name               = undef,
+  $redis_password             = undef,
+  $version                    = '1.4',
+  $logstash_input             = '',
+  $logstash_filter            = '',
+  $use_collectd               = true,
+  $collectd_disks             = ['dm-2'],
+  $use_dashboard              = true,
+  $dashboard_name             = 'host-baseboard',
+  $kibana_ip                  = '10.42.1.111',
+  $redis_ip                   = ["10.42.1.118","10.42.1.116","10.42.1.117"],
+  $host_specific              = undef,
+  $config_hash                = { 'LS_HEAP_SIZE' => '200m', 'LS_USER' => 'root', }
 ){
 
-  # Run Stages
   stage { 'pre':
   before => Stage["main"],
-  }
-
-  stage { 'after':
-  after => Stage["main"],
   }
 
   if ! defined(Class["role_analytics::logstash_indexer"]) {
@@ -53,28 +45,41 @@ class role_analytics::logstash_client2(
         purge_config          => true,
         stage                 => 'pre',
       }
-      class { 'collectd::plugin::load': }
-      class { 'collectd::plugin::memory': }
+      class { 'collectd::plugin::load':
+        stage                 => 'pre',
+      }
+      class { 'collectd::plugin::memory':
+        stage                 => 'pre',
+      }
       class { 'collectd::plugin::disk':
+        stage                 => 'pre',
         disks                 => $collectd_disks,
       }
-      class { 'collectd::plugin::interface': }
-      class { 'collectd::plugin::df': }
-      class { 'collectd::plugin::uptime': }
+      class { 'collectd::plugin::interface':
+        stage                 => 'pre',
+      }
+      class { 'collectd::plugin::df':
+        stage                 => 'pre',
+      }
+      class { 'collectd::plugin::uptime':
+        stage                 => 'pre',
+      }
       class { 'collectd::plugin::network':
-        timetolive    => '70',
-        maxpacketsize => '42',
-        forward       => false,
-        reportstats   => true,
-        servers       => { '127.0.0.1' => {
-          'port'          => '25826',
+        timetolive            => '70',
+        maxpacketsize         => '42',
+        forward               => false,
+        reportstats           => true,
+        servers               => { '127.0.0.1' => {
+          'port'              => '25826',
           },
         },
+        stage                 => 'pre',
       }
 
       class { 'collectd::plugin::logfile':
-        log_level => 'info',
-        log_file => '/var/log/collectd.log'
+        log_level             => 'info',
+        log_file              => '/var/log/collectd.log'
+        stage                 => 'pre',
       }
 
       file_fragment { 'input collectd':
@@ -86,16 +91,16 @@ class role_analytics::logstash_client2(
     }
 
     file_fragment { 'begin input':
-      tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
-      content               => 'input {
+      tag                     => "LS_CONFIG_CLIENT_${cluster_name}",
+      content                 => 'input {
       ',
-      order                 => 0,
+      order                   => 0,
       }
 
       file_fragment { 'input':
-        tag                     => "LS_CONFIG_CLIENT_${cluster_name}",
-        content                 => $logstash_input,
-        order                   => 200,
+        tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
+        content               => $logstash_input,
+        order                 => 200,
       }
 
       file_fragment { 'end_input':
@@ -103,20 +108,20 @@ class role_analytics::logstash_client2(
         content               => '
       }
       ',
-      order                 => 398,
+      order                   => 398,
     }
 
     file_fragment { 'begin filter':
-      tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
-      content               => 'filter {
+      tag                     => "LS_CONFIG_CLIENT_${cluster_name}",
+      content                 => 'filter {
       ',
-      order                 => 399,
+      order                   => 399,
       }
 
       file_fragment { 'filter':
-        tag                     => "LS_CONFIG_CLIENT_${cluster_name}",
-        content                 => $logstash_filter,
-        order                   => 500,
+        tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
+        content               => $logstash_filter,
+        order                 => 500,
       }
 
       file_fragment { 'end filter':
@@ -124,13 +129,13 @@ class role_analytics::logstash_client2(
         content               => '
       }
       ',
-      order                 => 698,
+      order                   => 698,
     }
 
     file_fragment { 'output':
-      tag                   => "LS_CONFIG_CLIENT_${cluster_name}",
-      content               => template('role_analytics/logstash_redis_output.erb'),
-      order                 => 699,
+      tag                     => "LS_CONFIG_CLIENT_${cluster_name}",
+      content                 => template('role_analytics/logstash_redis_output.erb'),
+      order                   => 699,
     }
 
     File_fragment <<| tag == "LS_CONFIG_CLIENT_${cluster_name}" |>> {
@@ -149,46 +154,43 @@ class role_analytics::logstash_client2(
     case $operatingsystem {
       'Ubuntu': {
         file_line { 'syslog_workaround':
-          ensure                  => "present",
-          require                 => Package['logstash'],
-          path                    => '/etc/init/logstash.conf',
-          match                   => 'setuid',
-          line                    => 'setuid root',
-          notify                  => [ Service["logstash"], Service["collectd"], ],
-          stage                   => 'after',
+          ensure              => "present",
+          require             => Package['logstash'],
+          path                => '/etc/init/logstash.conf',
+          match               => 'setuid',
+          line                => 'setuid root',
+          notify              => [ Service["logstash"], Service["collectd"], ],
         }
       }
       'CentOS': {
         file_line { 'syslog_workaround':
-          ensure                  => "present",
-          require                 => Package['logstash'],
-          path                    => '/etc/sysconfig/logstash',
-          match                   => 'LS_USER=',
-          line                    => 'LS_USER=root',
-          notify                  => [ Service["logstash"], Service["collectd"], ],
-          stage                   => 'after',
+          ensure              => "present",
+          require             => Package['logstash'],
+          path                => '/etc/sysconfig/logstash',
+          match               => 'LS_USER=',
+          line                => 'LS_USER=root',
+          notify              => [ Service["logstash"], Service["collectd"], ],
         }
       }
     }
 
     if $use_dashboard {
       file {"/tmp/${dashboard_name}.json":
-        ensure                    => "present",
-        path                      => "/tmp/${dashboard_name}.json",
-        mode                      => "644",
-        content                   => template("role_analytics/${dashboard_name}.json.erb"),
-        notify                    => Exec['install_dashboard'],
-        stage                     => 'after',
+        ensure                => "present",
+        path                  => "/tmp/${dashboard_name}.json",
+        mode                  => "644",
+        content               => template("role_analytics/${dashboard_name}.json.erb"),
+        notify                => Exec['install_dashboard'],
       }
       exec { 'install_dashboard':
-        command                   => "/usr/bin/curl -XPUT http://${kibana_ip}:9200/kibana-int/dashboard/host-${hostname} -T /tmp/${dashboard_name}.json",
-        refreshonly               => true,
+        command               => "/usr/bin/curl -XPUT http://${kibana_ip}:9200/kibana-int/dashboard/host-${hostname} -T /tmp/${dashboard_name}.json",
+        refreshonly           => true,
       }
     }
     else {
       file {"/tmp/${dashboard_name}.json":
-        ensure                  => absent,
-        path                    => "/tmp/${dashboard_name}.json",
+        ensure                => absent,
+        path                  => "/tmp/${dashboard_name}.json",
       }
     }
 }
