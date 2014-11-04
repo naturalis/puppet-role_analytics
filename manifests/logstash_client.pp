fb5b128a395f314ea1cf7f8f53e8519a54ab27f0
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-11-04 11:28:07 +0100

    Update logstash_client.pp

--- manifests/logstash_client.pp
+++ manifests/logstash_client.pp
@@ -53,25 +53,25 @@ class role_analytics::logstash_client(
             require           => Class ['role_analytics::collectd_repos'],
           }
 
-          file_line { 'syslog_workaround':
-            ensure            => 'present',
-            require           => Package['logstash'],
-            path              => '/etc/init/logstash.conf',
-            match             => 'setuid',
-            line              => 'setuid root',
-            notify            => [ Service['logstash'], Service['collectd'], ],
-          }
+          #file_line { 'syslog_workaround':
+          #  ensure            => 'present',
+          #  require           => Package['logstash'],
+          #  path              => '/etc/init/logstash.conf',
+          #  match             => 'setuid',
+          #  line              => 'setuid root',
+          #  notify            => [ Service['logstash'], Service['collectd'], ],
+          #}
 
-          if $memorysize_mb <= '512' {
-            file_line { 'set_heapsize':
-              ensure          => 'present',
-              require         => Package['logstash'],
-              path            => '/etc/init/logstash.conf',
-              match           => 'LS_HEAP_SIZE=',
-              line            => 'LS_HEAP_SIZE="100m"',
-              notify          => Service['logstash'],
-            }
-          }
+          #if $memorysize_mb <= '512' {
+          #  file_line { 'set_heapsize':
+          #    ensure          => 'present',
+          #    require         => Package['logstash'],
+          #    path            => '/etc/init/logstash.conf',
+          #    match           => 'LS_HEAP_SIZE=',
+          #    line            => 'LS_HEAP_SIZE="100m"',
+          #    notify          => Service['logstash'],
+          #  }
+          #}
         }
 
         redhat: {
